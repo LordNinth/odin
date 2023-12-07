6258312c9628a3b5d19686c496da5d2bacef836e
LordNinth <rr621801@gmail.com>
2023-12-07 01:34:26 +0000

    draft-1

---
+++ calculator/calc.js
@@ -0,0 +1,19 @@
+const addition = (a,b) =>{
+    return a + b 
+};
+
+const subtract = (a,b) => {
+    return a-b ;
+};
+
+const multiply = (a,b) => {
+    return a*b;
+};
+const divide = (a,b) =>{
+    return a/b;
+};
+
+// console.log(addition(2,3))
+// console.log(subtract(2,3))
+// console.log(multiply(2,3))
+// console.log(divide(6,3))
